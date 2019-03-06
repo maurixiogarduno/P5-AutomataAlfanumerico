68c84cb9914390697f52fea408cdea69d9c51ad3
Mauricio Garduño <mau.gar.gonz@icloud.com>
2019-03-06 04:13:21 -0600

    ya tengo sueño

---
+++ src/p5/automataalfanumerico/P5AutomataAlfanumerico.java
@@ -0,0 +1,20 @@
+package p5.automataalfanumerico;
+
+import GUI.Main;
+
+/**
+ *
+ * @author Jedidian
+ */
+public class P5AutomataAlfanumerico {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Main inicio = new Main();
+        inicio.setVisible(true);
+    }
+    
+}
